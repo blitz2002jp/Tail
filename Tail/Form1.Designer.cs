f8edb4388a552660e4e3e8ead95ea6dd646fde88
oeda <m.ooeda@eisansystem.jp>
2020-12-13 07:28:45 +0900

    first

---
+++ Tail/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Tail
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(27, 475);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox1.Location = new System.Drawing.Point(27, 22);
+            this.textBox1.MaxLength = 65535;
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(878, 394);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(801, 475);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 46);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.trackBar1.Location = new System.Drawing.Point(337, 460);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(242, 45);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(929, 533);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ Tail/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tail
+{
+    public partial class Form1 : Form
+    {
+        private string filePath = "";
+        private int startPos = -1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            filePath = SelectFile("", filePath);
+
+            int b = GetFileLineNum(filePath);
+
+
+            while (true)
+            {
+                var c = GetTail(filePath);
+
+                outTail(c);
+
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+
+        private void outTail(List<string> lines)
+        {
+            foreach (var item in lines)
+            {
+                //System.Diagnostics.Debug.Print(item);
+
+                this.textBox1.Text += item + Environment.NewLine; 
+            }
+
+            //カレット位置を末尾に移動
+            this.textBox1.SelectionStart = this.textBox1.Text.Length;
+            //テキストボックスにフォーカスを移動
+            this.textBox1.Focus();
+            //カレット位置までスクロール
+            this.textBox1.ScrollToCaret();
+
+            Application.DoEvents();
+        }
+
+
+        private string SelectFile(string defaultFileName = "", string defaultDir = @"C:\", string defaultFilter = "LOGファイル(*.log)|*.log|TXTファイル(*.txt)|*.txt|すべてのファイル(*.*)|*.*")
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.FileName = defaultFileName;
+            ofd.InitialDirectory = defaultDir;
+            ofd.Filter = defaultFilter;
+            ofd.FilterIndex = 1;
+            ofd.Title = "ファイルを選択してください";
+            //ダイアログボックスを閉じる前に現在のディレクトリを復元するようにする
+            ofd.RestoreDirectory = true;
+            //存在しないファイルの名前が指定されたとき警告を表示する
+            //デフォルトでTrueなので指定する必要はない
+            ofd.CheckFileExists = true;
+            //存在しないパスが指定されたとき警告を表示する
+            //デフォルトでTrueなので指定する必要はない
+            ofd.CheckPathExists = true;
+
+            //ダイアログを表示する
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                return ofd.FileName;
+            }
+
+            return "";
+        }
+
+
+        private int GetFileLineNum(string fileName)
+        {
+            var lineCount = 0;
+
+            using (System.IO.StreamReader sr = OpenFile(fileName))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    lineCount++;
+                }
+            }
+
+            return lineCount;
+        }
+
+
+        private List<string> GetTail(string fileName)
+        {
+            var res = new List<string>();
+            var lineCount = 0;
+            var maxLine = GetFileLineNum(fileName);
+
+            using (System.IO.StreamReader sr = OpenFile(fileName))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if( this.startPos <= lineCount)
+                    {
+                        res.Add(line);
+                    }
+
+                    lineCount++;
+                }
+
+                this.startPos = lineCount;
+            }
+
+            return res;
+        }
+
+        private System.IO.StreamReader OpenFile(string fileName)
+        {
+            System.IO.FileStream fs = new System.IO.FileStream(
+                fileName,
+                System.IO.FileMode.Open,
+                System.IO.FileAccess.Read,
+                System.IO.FileShare.ReadWrite);
+
+            return new System.IO.StreamReader(fs);
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            var sz = 16 + ((System.Windows.Forms.TrackBar)sender).Value;
+
+            this.textBox1.Font = new System.Drawing.Font(this.textBox1.Font.FontFamily, sz, this.textBox1.Font.Style);
+        }
+    }
+}
